1f905983fa79f06a4c83c0249e85de32cbb3c5f7
hari-0330 <124655744+hari-0330@users.noreply.github.com>
2025-06-25 16:32:17 +0530

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import admin from 'firebase-admin';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-export default admin; 
+export default admin;
